f1c3537f76e12dec42c65e7bc935dc22f3a3e234
ayan4m1 <andrew@bulletlogic.com>
2019-06-06 07:23:15 -0400

    add tests for login form handling

--- src/pages/Login.test.js
+++ src/pages/Login.test.js
@@ -3,16 +3,19 @@ import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 
-import Login from './Login';
+import ConnectedLogin, { Login } from './Login';
 import { withMemoryRouter } from 'utils';
 
 describe('<Login />', () => {
+  const actions = {
+    loginUser: jest.fn()
+  };
   const initialState = {};
   const mockStore = configureStore();
   const store = mockStore(initialState);
+  const RoutedLogin = withMemoryRouter(ConnectedLogin);
 
   it('renders correctly', () => {
-    const RoutedLogin = withMemoryRouter(Login);
     const component = renderer.create(
       <Provider store={store}>
         <RoutedLogin />
@@ -21,4 +24,33 @@ describe('<Login />', () => {
 
     expect(component.toJSON()).toMatchSnapshot();
   });
+
+  it('can handleSubmit', () => {
+    const emailAddress = 'some@one.org';
+    const password = 'testing';
+    const mockEvent = {
+      preventDefault: jest.fn()
+    };
+    const component = renderer.create(<Login actions={actions} />);
+    const { instance } = component.root.findByType(Login);
+
+    expect(instance).toBeDefined();
+    instance.handleTextChange({
+      target: {
+        name: 'emailAddress',
+        value: emailAddress
+      }
+    });
+    instance.handleTextChange({
+      target: {
+        name: 'password',
+        value: password
+      }
+    });
+    instance.handleSubmit(mockEvent);
+    expect(actions.loginUser).toHaveBeenCalledWith(emailAddress, password);
+    expect(mockEvent.preventDefault).toHaveBeenCalled();
+  });
+
+  it('can handleTextChange', () => {});
 });
