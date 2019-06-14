ee13301e22d20fc039bcc3d7ff58419ba4817e6b
Josef Bud <josefbud@gmail.com>
2019-06-14 19:58:40 -0400

    Merged Home.js with the one in the correct location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from '@reach/router';
+
+import TopRecipesDay from 'components/templates/TopRecipesDay';
+import NewRecipes from 'components/templates/NewRecipes';
+import FeaturedMixer from 'components/templates/FeaturedMixer';
 
 export default class Home extends Component {
   render() {
     return (
       <Container>
-        <h1>Home</h1>
+        <Row className="text-center">
+          <Col>
+            <h1>Welcome to MixNJuice!</h1>
+            <p style={{ 'font-size': '0.8em' }}>
+              <Link to="#">Click here to customize your front page!</Link>
+            </p>
+          </Col>
+        </Row>
+        <TopRecipesDay />
+        <hr />
+        <NewRecipes />
+        <hr />
+        <FeaturedMixer />
       </Container>
     );
   }
