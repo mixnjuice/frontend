144152b9a113cc4c43ca057e49fc657326e8efdc
Josef Bud <josefbud@gmail.com>
2019-06-28 16:55:38 -0400

    Rudimentary beginning of user recipes page

--- src/pages/user/Recipes.js
+++ src/pages/user/Recipes.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-
-import { Container } from 'react-bootstrap';
+import { Link } from '@reach/router';
+import { Container, Row, Col, Table } from 'react-bootstrap';
 
 export default class UserRecipes extends Component {
+  listRecipes() {}
   render() {
+    const listItems = [];
+
+    for (let i = 0; i < 20; i++) {
+      listItems.push(
+        <tr>
+          <Link to="#">Recipe {i}</Link>
+        </tr>
+      );
+    }
+
     return (
-      <Container>
-        <h1>User Recipes</h1>
+      <Container className="text-center">
+        <Row>
+          <Col>
+            <h1>Your Original Recipes</h1>
+            <Table striped bordered hover>
+              <tbody>{listItems}</tbody>
+            </Table>
+          </Col>
+          <Col>
+            <h1>Your Adapted Recipes</h1>
+            <Table striped bordered hover>
+              <tbody>{listItems}</tbody>
+            </Table>
+          </Col>
+        </Row>
       </Container>
     );
   }
