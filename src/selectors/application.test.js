e8541d73376580ae994499b2be5f87da07d08fd4
ayan4m1 <andrew@bulletlogic.com>
2019-07-05 21:54:24 -0400

    add application selectors and tests for them

--- src/selectors/application.test.js
+++ src/selectors/application.test.js
@@ -1,13 +1,43 @@
-import { getApplication } from './application';
+import { initialState } from 'reducers/application';
+import {
+  getApplication,
+  isLoggingIn,
+  isLoggingOut,
+  getUser,
+  getError,
+  getToken,
+  getTokenExpiration
+} from './application';
 
 describe('application selectors', () => {
-  const state = {
-    application: {
-      test: true
-    }
-  };
+  const state = initialState;
+  const { authorization: authState } = initialState;
 
-  it('getApplication', () => {
+  it('can getApplication', () => {
     expect(getApplication(state)).toBe(state.application);
+  });
+
+  it('can get isLoggingIn', () => {
+    expect(isLoggingIn(state)).toBe(state.isLoggingIn);
+  });
+
+  it('can get isLoggingOut', () => {
+    expect(isLoggingOut(state)).toBe(state.isLoggingOut);
+  });
+
+  it('can getUser', () => {
+    expect(getUser(state)).toBe(state.user);
+  });
+
+  it('can getError', () => {
+    expect(getError(state)).toBe(state.error);
+  });
+
+  it('can getToken', () => {
+    expect(getToken(state)).toBe(authState.accessToken);
+  });
+
+  it('can getTokenExpiration', () => {
+    expect(getTokenExpiration(state)).toBe(authState.expiration);
   });
 });
