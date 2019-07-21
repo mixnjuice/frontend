a983fd7ed2972f3110459e4c14c97a568ea0637c
Josef Bud <josefbud@gmail.com>
2019-07-20 21:18:28 -0400

    Added star icon to library

--- src/icons.js
+++ src/icons.js
@@ -2,10 +2,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faFire,
   faUser,
-  faHeart as fasHeart
+  faHeart as fasHeart,
+  faStar as fasStar
 } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+import {
+  faHeart as farHeart,
+  faStar as farStar
+} from '@fortawesome/free-regular-svg-icons';
 
-library.add(faFire, faUser, fasHeart, farHeart);
+library.add(faFire, faUser, fasHeart, farHeart, fasStar, farStar);
 
 export default library;
