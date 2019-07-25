3b3248e7bbd4896d7074e2c7c109c0afcd854166
ayan4m1 <andrew@bulletlogic.com>
2019-07-25 13:11:40 -0400

    try to fix code coverage generation

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', '!src/*.js'],
   coverageDirectory: './coverage',
-  coverageReporters: ['html', 'text-summary'],
+  coverageReporters: ['html', 'text-summary', 'lcov'],
   modulePaths: ['<rootDir>/src/'],
   globals: {
     API_URL: 'http://localhost:3000'
