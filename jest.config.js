f3069ddd00dcb64795ddbf5fe7df9007b3be0ed6
ayan4m1 <andrew@bulletlogic.com>
2019-07-27 12:00:33 -0400

    add jest-localstorage-mock

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js', '!src/*.js'],
   coverageDirectory: './coverage',
   coverageReporters: ['html', 'text-summary', 'lcov'],
+  setupFiles: ['jest-localstorage-mock'],
   modulePaths: ['<rootDir>/src/'],
   globals: {
     API_URL: 'http://localhost:3000'
