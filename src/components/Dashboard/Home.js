80f4a4a36e746e07e71c7e4d4db7ec362adbdbb6
David Dyess II <david.dyess@gmail.com>
2019-08-12 10:58:27 -0600

    Console warning for malformed html fix

--- src/components/Dashboard/Home.js
+++ src/components/Dashboard/Home.js
@@ -44,41 +44,31 @@ export class Home extends Component {
         <Card bg="danger" text="white">
           <Card.Header>Users: {users}</Card.Header>
           <Card.Body>
-            <Card.Text>
-              <h3>Activated Users: {activatedUsers}</h3>
-              <h3>Inactive Users: {users - activatedUsers}</h3>
-              <h3>Active Tokens: {userTokens}</h3>
-            </Card.Text>
+            <h3>Activated Users: {activatedUsers}</h3>
+            <h3>Inactive Users: {users - activatedUsers}</h3>
+            <h3>Active Tokens: {userTokens}</h3>
           </Card.Body>
         </Card>
         <Card bg="primary" text="white">
           <Card.Body>
-            <Card.Text>
-              <h2>Tags: {tags}</h2>
-            </Card.Text>
+            <h2>Tags: {tags}</h2>
           </Card.Body>
         </Card>
         <Card bg="secondary" text="white">
           <Card.Body>
-            <Card.Text>
-              <h2>Vendors: {vendors}</h2>
-            </Card.Text>
+            <h2>Vendors: {vendors}</h2>
           </Card.Body>
         </Card>
         <Card bg="info" text="white">
           <Card.Header>Flavors: {flavors}</Card.Header>
           <Card.Body>
-            <Card.Text>
-              <h2>Tags: {flavorTags}</h2>
-            </Card.Text>
+            <h2>Tags: {flavorTags}</h2>
           </Card.Body>
         </Card>
         <Card bg="warning" text="white">
           <Card.Header>Recipes: {recipes}</Card.Header>
           <Card.Body>
-            <Card.Text>
-              <h2>Tags: {recipeTags}</h2>
-            </Card.Text>
+            <h2>Tags: {recipeTags}</h2>
           </Card.Body>
         </Card>
       </CardColumns>
