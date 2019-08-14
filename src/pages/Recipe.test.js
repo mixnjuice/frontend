656eb441061565edeaeb7f672f2c2ae467feb3f8
JosefBud <josefbud@gmail.com>
2019-08-14 12:20:21 -0400

    Updated test & snapshot

--- src/pages/Recipe.test.js
+++ src/pages/Recipe.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
 
-import Recipe from './Recipe';
+import { initialState } from 'reducers/application';
+import { Recipe } from './Recipe';
 import recipes from '../data/recipes.json';
+
+const mockStore = configureStore();
+const store = mockStore({ application: initialState });
 
 describe('<Recipe />', () => {
   const actions = {
@@ -15,7 +21,11 @@ describe('<Recipe />', () => {
   });
 
   it('renders correctly', () => {
-    const component = renderer.create(<Recipe {...props('?id=1')} />);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Recipe {...props('?id=1')} />
+      </Provider>
+    );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
@@ -28,6 +38,12 @@ describe('<Recipe />', () => {
 
     expect(instance).toBeDefined();
     instance.findRecipe();
-    expect(instance.state).toEqual({ ...recipes[0], flavorTotal: 9 });
+    expect(instance.state).toEqual({
+      ...recipes[0],
+      flavorTotal: 9,
+      favoriteIcon: ['far', 'heart'],
+      favorited: false,
+      rating: 0
+    });
   });
 });
