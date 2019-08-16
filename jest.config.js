336dea02e13b2eece3840ce004126834fa2c8d22
Andrew DeLisa <andrew@bulletlogic.com>
2019-08-16 08:55:58 -0400

    make prop-type warnings during test execution cause test failures (#34)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js', '!src/*.js'],
   coverageDirectory: './coverage',
   coverageReporters: ['html', 'text-summary', 'lcov'],
-  setupFiles: ['jest-localstorage-mock'],
+  setupFiles: ['jest-localstorage-mock', 'jest-prop-type-error'],
   modulePaths: ['<rootDir>/src/'],
   globals: {
     API_URL: 'http://localhost:3000'
