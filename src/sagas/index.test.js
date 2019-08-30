8803121b415db44826694e45e40946a1fb8cebdf
ayan4m1 <andrew@bulletlogic.com>
2019-08-30 12:10:07 -0400

    fix saga test to use new recipe sagas

--- src/sagas/index.test.js
+++ src/sagas/index.test.js
@@ -2,6 +2,7 @@ import { fork, all } from 'redux-saga/effects';
 
 import saga from './index';
 import toast from './toast';
+import recipe from './recipe';
 import application from './application';
 
 describe('index saga', () => {
@@ -9,6 +10,6 @@ describe('index saga', () => {
     const gen = saga();
     const result = gen.next();
 
-    expect(result.value).toEqual(all([application, toast].map(fork)));
+    expect(result.value).toEqual(all([application, toast, recipe].map(fork)));
   });
 });
