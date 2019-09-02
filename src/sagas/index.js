e4d4b2c4e725552e94f5559942c13abc547ed447
ayan4m1 <andrew@bulletlogic.com>
2019-09-02 10:59:36 -0400

    add flavor reducer/sagas/selectors for fetching stash

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,8 +2,9 @@ import { all, fork } from 'redux-saga/effects';
 
 import application from './application';
 import toast from './toast';
+import flavor from './flavor';
 import recipe from './recipe';
 
 export default function* saga() {
-  yield all([application, toast, recipe].map(fork));
+  yield all([application, toast, flavor, recipe].map(fork));
 }
