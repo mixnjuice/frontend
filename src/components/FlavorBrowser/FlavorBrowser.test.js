84dfd2d9ec26c8b100b7a1ba632215fd048aaa7a
ayan4m1 <andrew@bulletlogic.com>
2019-09-25 09:33:09 -0400

    add FlavorBrowser tests

---
+++ src/components/FlavorBrowser/FlavorBrowser.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { FlavorBrowser } from './FlavorBrowser';
+
+describe('<FlavorBrowser />', () => {
+  const actions = {
+    requestStash: jest.fn(),
+    setRecipeIngredients: jest.fn()
+  };
+  const stash = [
+    {
+      Flavor: {
+        name: 'Test',
+        id: '123',
+        Vendor: { code: 'CAP', name: 'Capella' }
+      }
+    },
+    {
+      Flavor: {
+        name: 'Another',
+        id: '456',
+        Vendor: { code: 'FLV', name: 'Flavorah' }
+      }
+    }
+  ];
+  const recipe = {
+    ingredients: []
+  };
+  const props = {
+    stashLoaded: true,
+    actions,
+    recipe,
+    stash
+  };
+
+  beforeEach(() => {
+    actions.requestStash.mockReset();
+    actions.setRecipeIngredients.mockReset();
+  });
+
+  it('renders correctly', () => {
+    const component = renderer.create(<FlavorBrowser {...props} />);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('calls requestStash if stash is not loaded', () => {
+    renderer.create(<FlavorBrowser actions={actions} stashLoaded={false} />);
+
+    expect(actions.requestStash).toHaveBeenCalled();
+  });
+});
