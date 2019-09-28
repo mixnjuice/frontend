9c80cd59f8ce599fa4331ec66070cbf185c5f56a
ayan4m1 <andrew@bulletlogic.com>
2019-09-27 21:12:59 -0400

    fix test warnings for SplitSlider

--- src/components/SplitSlider/SplitSlider.test.js
+++ src/components/SplitSlider/SplitSlider.test.js
@@ -5,7 +5,11 @@ import SplitSlider from './SplitSlider';
 
 describe('<SplitSlider />', () => {
   it('renders component correctly', () => {
-    const tree = renderer.create(<SplitSlider initialValue={50} />).toJSON();
+    const tree = renderer
+      .create(
+        <SplitSlider name="test" initialValue={50} onChange={jest.fn()} />
+      )
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
