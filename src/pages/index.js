0cf50aaa1a13fd1049d8fc4a57623b58d3e64517
ayan4m1 <andrew@bulletlogic.com>
2020-01-12 22:02:39 -0500

    refactor pages module to be testable, add test suite

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,38 @@
 import { lazy } from 'react';
 
-export const Home = lazy(() =>
-  import(/* webpackChunkName: "home" */ 'pages/Home')
-);
-export const Login = lazy(() =>
-  import(/* webpackChunkName: "home" */ 'pages/Login')
-);
-export const NotFound = lazy(() =>
-  import(/* webpackChunkName: "home" */ 'pages/NotFound')
-);
-export const Register = lazy(() =>
-  import(/* webpackChunkName: "home" */ 'pages/Register')
-);
-export const Profile = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/Profile')
-);
-export const UserRecipes = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/Recipes')
-);
-export const Favorites = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/Favorites')
-);
-export const FlavorStash = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/FlavorStash')
-);
-export const UserSettings = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/Settings')
-);
-export const ShoppingList = lazy(() =>
-  import(/* webpackChunkName: "user" */ 'pages/user/ShoppingList')
-);
-export const Recipes = lazy(() =>
-  import(/* webpackChunkName: "recipe" */ 'pages/Recipes')
-);
-export const Calculator = lazy(() =>
-  import(/* webpackChunkName: "recipe" */ 'pages/Calculator')
-);
-export const Flavors = lazy(() =>
-  import(/* webpackChunkName: "recipe" */ 'pages/Flavors')
-);
-export const Recipe = lazy(() =>
-  import(/* webpackChunkName: "recipe" */ 'pages/Recipe')
-);
+export const imports = {
+  Calculator: () => import(/* webpackChunkName: "recipe" */ 'pages/Calculator'),
+  Favorites: () =>
+    import(/* webpackChunkName: "user" */ 'pages/user/Favorites'),
+  Flavors: () => import(/* webpackChunkName: "recipe" */ 'pages/Flavors'),
+  FlavorStash: () =>
+    import(/* webpackChunkName: "user" */ 'pages/user/FlavorStash'),
+  Home: () => import(/* webpackChunkName: "home" */ 'pages/Home'),
+  Login: () => import(/* webpackChunkName: "home" */ 'pages/Login'),
+  NotFound: () => import(/* webpackChunkName: "home" */ 'pages/NotFound'),
+  Profile: () => import(/* webpackChunkName: "user" */ 'pages/user/Profile'),
+  Recipe: () => import(/* webpackChunkName: "recipe" */ 'pages/Recipe'),
+  Recipes: () => import(/* webpackChunkName: "recipe" */ 'pages/Recipes'),
+  Register: () => import(/* webpackChunkName: "home" */ 'pages/Register'),
+  ShoppingList: () =>
+    import(/* webpackChunkName: "user" */ 'pages/user/ShoppingList'),
+  UserRecipes: () =>
+    import(/* webpackChunkName: "user" */ 'pages/user/Recipes'),
+  UserSettings: () =>
+    import(/* webpackChunkName: "user" */ 'pages/user/Settings')
+};
+
+export const Calculator = lazy(imports.Calculator);
+export const Favorites = lazy(imports.Favorites);
+export const Flavors = lazy(imports.Flavors);
+export const FlavorStash = lazy(imports.FlavorStash);
+export const Home = lazy(imports.Home);
+export const Login = lazy(imports.Login);
+export const NotFound = lazy(imports.NotFound);
+export const Profile = lazy(imports.Profile);
+export const Recipe = lazy(imports.Recipe);
+export const Recipes = lazy(imports.Recipes);
+export const Register = lazy(imports.Register);
+export const ShoppingList = lazy(imports.ShoppingList);
+export const UserRecipes = lazy(imports.UserRecipes);
+export const UserSettings = lazy(imports.UserSettings);
