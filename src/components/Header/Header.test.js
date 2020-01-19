c336ca5c11bde2fa2a7e919edd3b21a2c102c57e
daviddyess <david.dyess@gmail.com>
2020-01-19 14:13:19 -0700

    Test logout in Header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
-
 import { Header } from './Header';
-import { initialState } from 'reducers/application';
 import { withMemoryRouter } from 'utils';
 // Prevent findDOMNode error in test from Dropdown component in react-bootstrap
 jest.mock('react-dom', () => ({
@@ -15,8 +11,6 @@ describe('<Header />', () => {
   const actions = {
     logoutUser: jest.fn()
   };
-  const mockStore = configureStore();
-  const store = mockStore({ application: initialState });
   const RoutedHeader = withMemoryRouter(Header);
 
   it('renders logged out correctly', () => {
@@ -43,9 +37,7 @@ describe('<Header />', () => {
 
   it('can logoutUser', () => {
     const component = renderer.create(
-      <Provider store={store}>
-        <RoutedHeader actions={actions} loggedIn={true} />
-      </Provider>
+      <RoutedHeader actions={actions} loggedIn={true} />
     );
     const { instance } = component.root.findByType(Header);
 
