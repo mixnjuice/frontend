0964828f95c2ea31ff9d0ba6989533b78d25581d
daviddyess <david.dyess@gmail.com>
2020-02-09 02:15:06 -0700

    Reverting pagination test for now

--- src/components/Pagination.t.est.js
+++ src/components/Pagination.t.est.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
@@ -75,4 +75,4 @@ describe('Pagination', () => {
     expect(instance).toBeDefined();
     instance.updateLimit();
   });
-});
+}); */
