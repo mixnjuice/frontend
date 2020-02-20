61c7526b2fca10786b7beb2b7ad8023f7e987488
daviddyess <david.dyess@gmail.com>
2020-02-19 22:44:37 -0700

    Updated Pagination HOC test

--- src/components/Pagination/Pagination.test.js
+++ src/components/Pagination/Pagination.test.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
+// import withPagination, { pagination, mapStateToProps, mapDispatchToProps } from './Pagination';
+import { pagination } from './Pagination';
+// Prevent findDOMNode error in test from Dropdown component in react-bootstrap
+jest.mock('react-dom', () => ({
+  findDOMNode: () => ({})
+}));
 
-import { initialState } from 'reducers/flavors';
-import ConnectedPagination, { withPagination } from './Pagination';
-import { withMemoryRouter } from 'utils';
-
-describe('Pagination', () => {
-  const mockStore = configureStore();
-  const store = mockStore({ flavors: initialState, page: 1 });
+describe('<Pagination />', () => {
   const actions = {
-    requestFlavors: jest.fn()
+    action: jest.fn()
   };
 
   const page = {
@@ -25,54 +23,82 @@ describe('Pagination', () => {
       value: 40
     }
   };
+  const collection = [
+    {
+      id: '1',
+      vendorId: 3,
+      name: '27 Bears',
+      slug: null,
+      density: null,
+      Vendor: { id: 3, name: 'Capella', slug: 'capella', code: 'CAP' }
+    },
+    {
+      id: '2',
+      vendorId: 3,
+      name: '27 Fish',
+      slug: null,
+      density: null,
+      Vendor: { id: 3, name: 'Capella', slug: 'capella', code: 'CAP' }
+    },
+    {
+      id: '3',
+      vendorId: 3,
+      name: 'Acai',
+      slug: null,
+      density: null,
+      Vendor: { id: 3, name: 'Capella', slug: 'capella', code: 'CAP' }
+    },
+    {
+      id: '4',
+      vendorId: 3,
+      name: 'Amaretto',
+      slug: null,
+      density: null,
+      Vendor: { id: 3, name: 'Capella', slug: 'capella', code: 'CAP' }
+    },
+    {
+      id: '5',
+      vendorId: 3,
+      name: 'Anise',
+      slug: null,
+      density: null,
+      Vendor: { id: 3, name: 'Capella', slug: 'capella', code: 'CAP' }
+    }
+  ];
+  const pager = { count: 861, limit: 5, page: 1, pages: 173 };
+  const TestComponent = () => <p />;
+  const Component = pagination(TestComponent);
+  // const ConnectedComponent = withPagination(TestComponent);
+  const props = {
+    actions,
+    pager,
+    collection
+  };
 
-  const RoutedPagination = withMemoryRouter(ConnectedPagination);
+  const component = renderer.create(<Component {...props} />);
+  const { instance } = component.root.findByType(Component);
+
+  instance.setState = jest.fn();
 
   it('renders correctly', () => {
     expect(
-      renderer
-        .create(
-          <Provider store={store}>
-            <RoutedPagination />
-          </Provider>
-        )
-        .toJSON()
+      renderer.create(<Component {...props} />).toJSON()
     ).toMatchSnapshot();
+    expect(instance).toBeDefined();
   });
 
   it('can changePage', () => {
-    const component = renderer.create(
-      <Provider store={store}>
-        <RoutedPagination actions={actions} />
-      </Provider>
-    );
-    const { instance } = component.root.findByType(RoutedPagination);
-
-    expect(instance).toBeDefined();
     instance.changePage(page);
+    expect(actions.action).toHaveBeenCalledWith({ ...pager, page: 1 });
   });
 
   it('can changeLimit', () => {
-    const component = renderer.create(
-      <Provider store={store}>
-        <RoutedPagination actions={actions} />
-      </Provider>
-    );
-    const { instance } = component.root.findByType(withPagination);
-
-    expect(instance).toBeDefined();
     instance.changeLimit(limit);
+    expect(instance.setState).toHaveBeenCalledWith({ limit: 40 });
   });
 
   it('can updateLimit', () => {
-    const component = renderer.create(
-      <Provider store={store}>
-        <RoutedPagination actions={actions} />
-      </Provider>
-    );
-    const { instance } = component.root.findByType(withPagination);
-
-    expect(instance).toBeDefined();
     instance.updateLimit();
+    expect(actions.action).toHaveBeenCalledWith({ page: 1, limit: 20 });
   });
 });
