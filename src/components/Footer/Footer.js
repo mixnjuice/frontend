97d63a7d97b92cd28ed0a9efef8aa43d8837193c
JosefBud <josefbud@gmail.com>
2020-03-01 20:00:10 -0500

    Making the footer stick to the bottom of the page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="mt-4 py-2 page-footer text-right border-top">
+      <footer className="footer mt-auto py-2 page-footer text-right border-top">
         <small>
           Copyright &copy; {new Date().getFullYear()} mixnjuice.com. Recipes are
           property of their respective creators.
