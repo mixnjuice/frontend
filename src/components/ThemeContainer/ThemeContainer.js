0bd8f2e0b2e3d618cf5448853e34418436a4491c
N.S <limeorange@protonmail.com>
2020-03-25 19:16:34 +0100

    add ThemeContainer tests

--- src/components/ThemeContainer/ThemeContainer.js
+++ src/components/ThemeContainer/ThemeContainer.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 export class ThemeContainer extends Component {
   static propTypes = {
-    theme: PropTypes.string
+    theme: PropTypes.string.isRequired
   };
 
   constructor(props) {
