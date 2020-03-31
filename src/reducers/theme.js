527fe9c9de7a72539cbd3c733f2c2063c68df822
N.S <limeorange@protonmail.com>
2020-03-31 18:19:18 +0200

    remove setTheme reducer

--- src/reducers/theme.js
+++ src/reducers/theme.js
@@ -1,18 +1,12 @@
 import { buildActions } from 'utils';
 
-export const types = buildActions('theme', ['SET_THEME', 'TOGGLE_DARK_MODE']);
-
-const setTheme = theme => ({
-  type: types.SET_THEME,
-  theme
-});
+export const types = buildActions('theme', ['TOGGLE_DARK_MODE']);
 
 const toggleDarkMode = () => ({
   type: types.TOGGLE_DARK_MODE
 });
 
 export const actions = {
-  setTheme,
   toggleDarkMode
 };
 
@@ -22,11 +16,6 @@ export const initialState = {
 
 export const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case types.SET_THEME:
-      return {
-        ...state,
-        name: action.theme
-      };
     case types.TOGGLE_DARK_MODE:
       if (state.name === 'default') {
         return {
--- src/reducers/theme.test.js
+++ src/reducers/theme.test.js
@@ -3,21 +3,6 @@ import { reducer, types, actions } from './theme';
 describe('theme reducer', () => {
   const name = 'default';
 
-  it('has SET_THEME action', () => {
-    expect(actions.setTheme(name)).toEqual({
-      type: types.SET_THEME,
-      theme: name
-    });
-  });
-
-  it('reduces SET_THEME action', () => {
-    const action = actions.setTheme(name);
-
-    expect(reducer({}, action)).toEqual({
-      name
-    });
-  });
-
   it('has TOGGLE_DARK_MODE action', () => {
     expect(actions.toggleDarkMode()).toEqual({
       type: types.TOGGLE_DARK_MODE
