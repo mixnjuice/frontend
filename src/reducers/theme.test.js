66bcc8e74f2343e98df4223a46e2abc9b87098f2
N.S <limeorange@protonmail.com>
2020-04-17 18:48:34 +0200

    update theme reducer test to test for actual functionality

--- src/reducers/theme.test.js
+++ src/reducers/theme.test.js
@@ -1,8 +1,6 @@
 import { reducer, types, actions } from './theme';
 
 describe('theme reducer', () => {
-  const name = 'default';
-
   it('has TOGGLE_DARK_MODE action', () => {
     expect(actions.toggleDarkMode()).toEqual({
       type: types.TOGGLE_DARK_MODE
@@ -12,6 +10,8 @@ describe('theme reducer', () => {
   it('reduces TOGGLE_DARK_MODE action', () => {
     const action = actions.toggleDarkMode();
 
-    expect(reducer({}, action)).toEqual({ name });
+    expect(reducer({}, action)).toEqual({ name: 'default' });
+    expect(reducer({ name: 'default' }, action)).toEqual({ name: 'dark' });
+    expect(reducer({ name: 'dark' }, action)).toEqual({ name: 'default' });
   });
 });
