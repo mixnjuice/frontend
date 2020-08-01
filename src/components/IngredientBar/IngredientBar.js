81f9c98f47d31af2d6f2e6a65b8f5eba7b60cac7
ayan4m1 <andrew@bulletlogic.com>
2020-08-01 12:26:44 -0400

    continue porting components to functional style

--- src/components/IngredientBar/IngredientBar.js
+++ src/components/IngredientBar/IngredientBar.js
@@ -1,109 +1,118 @@
 import PropTypes from 'prop-types';
-import React, { Component, Fragment, createRef } from 'react';
+import React, { Fragment, useRef, useState, useCallback } from 'react';
 import { ProgressBar, Tooltip, Overlay } from 'react-bootstrap';
 
-export default class IngredientBar extends Component {
-  static propTypes = {
-    nicotine: PropTypes.number.isRequired,
-    flavor: PropTypes.number.isRequired,
-    vg: PropTypes.number.isRequired,
-    pg: PropTypes.number.isRequired
+export default function IngredientBar(props) {
+  const [nicotineVisible, setNicotineVisible] = useState(false);
+  const [flavorVisible, setFlavorVisible] = useState(false);
+  const [vgVisible, setVgVisible] = useState(false);
+  const [pgVisible, setPgVisible] = useState(false);
+  const components = {
+    nicotine: {
+      ref: useRef(),
+      getter: () => nicotineVisible,
+      setter: setNicotineVisible
+    },
+    flavor: {
+      ref: useRef(),
+      getter: () => flavorVisible,
+      setter: setFlavorVisible
+    },
+    vg: {
+      ref: useRef(),
+      getter: () => vgVisible,
+      setter: setVgVisible
+    },
+    pg: {
+      ref: useRef(),
+      getter: () => pgVisible,
+      setter: setPgVisible
+    }
   };
+  const setTooltipVisibility = useCallback(
+    (name, value) => components[name].setter(value),
+    [components]
+  );
+  const getTooltip = useCallback(
+    (name) => {
+      const { [name]: value } = props;
+      const {
+        [name]: {
+          ref: { current: ref },
+          getter
+        }
+      } = components;
+      const show = getter();
 
-  static defaultProps = {
-    nicotine: 0,
-    flavor: 0,
-    vg: 0,
-    pg: 0
-  };
+      return (
+        <Overlay target={ref} show={show} placement="top">
+          <Tooltip id={`${name}-tooltip`}>{Math.round(value)}%</Tooltip>
+        </Overlay>
+      );
+    },
+    [components, props]
+  );
 
-  constructor(props) {
-    super(props);
+  const { nicotine, flavor, vg, pg } = props;
 
-    this.refList = {
-      nicotine: createRef(),
-      flavor: createRef(),
-      vg: createRef(),
-      pg: createRef()
-    };
-    this.state = {
-      nicotine: false,
-      flavor: false,
-      vg: false,
-      pg: false
-    };
-    this.toggleTooltipVisibility = this.toggleTooltipVisibility.bind(this);
-  }
+  const nicotineLabel = nicotine > 5 ? `Nic (${Math.round(nicotine)}%)` : 'Nic';
+  const flavorLabel = flavor > 5 ? `Flavor (${Math.round(flavor)}%)` : 'Flv';
+  const vgLabel = vg > 5 ? `VG (${Math.round(vg)}%)` : 'VG';
+  const pgLabel = pg > 5 ? `PG (${Math.round(pg)}%)` : 'PG';
 
-  toggleTooltipVisibility(name) {
-    this.setState({
-      [name]: !this.state[name]
-    });
-  }
-
-  tooltip(name) {
-    const { [name]: show } = this.state;
-    const { [name]: value } = this.props;
-    const {
-      [name]: { current: ref }
-    } = this.refList;
-
-    return (
-      <Overlay target={ref} show={show} placement="top">
-        <Tooltip id={`${name}-tooltip`}>{Math.round(value)}%</Tooltip>
-      </Overlay>
-    );
-  }
-
-  render() {
-    const { nicotine, flavor, vg, pg } = this.props;
-
-    const nicotineLabel =
-      nicotine > 5 ? `Nic (${Math.round(nicotine)}%)` : 'Nic';
-    const flavorLabel = flavor > 5 ? `Flavor (${Math.round(flavor)}%)` : 'Flv';
-    const vgLabel = vg > 5 ? `VG (${Math.round(vg)}%)` : 'VG';
-    const pgLabel = pg > 5 ? `PG (${Math.round(pg)}%)` : 'PG';
-
-    return (
-      <Fragment>
-        <ProgressBar className="ingredient-bar">
-          <ProgressBar
-            now={nicotine}
-            variant="danger"
-            label={nicotineLabel}
-            ref={this.refList.nicotine}
-            onMouseEnter={() => this.toggleTooltipVisibility('nicotine')}
-            onMouseLeave={() => this.toggleTooltipVisibility('nicotine')}
-          />
-          <ProgressBar
-            now={flavor}
-            label={flavorLabel}
-            ref={this.refList.flavor}
-            onMouseEnter={() => this.toggleTooltipVisibility('flavor')}
-            onMouseLeave={() => this.toggleTooltipVisibility('flavor')}
-          />
-          <ProgressBar
-            now={vg}
-            variant="success"
-            label={vgLabel}
-            ref={this.refList.vg}
-            onMouseEnter={() => this.toggleTooltipVisibility('vg')}
-            onMouseLeave={() => this.toggleTooltipVisibility('vg')}
-          />
-          <ProgressBar
-            now={pg}
-            variant="warning"
-            label={pgLabel}
-            ref={this.refList.pg}
-            onMouseEnter={() => this.toggleTooltipVisibility('pg')}
-            onMouseLeave={() => this.toggleTooltipVisibility('pg')}
-          />
-        </ProgressBar>
-        {this.tooltip('nicotine')}
-        {this.tooltip('flavor')}
-        {this.tooltip('vg')}
-        {this.tooltip('pg')}
-      </Fragment>
-    );
-  }
+  return (
+    <Fragment>
+      <ProgressBar className="ingredient-bar">
+        <ProgressBar
+          now={nicotine}
+          variant="danger"
+          label={nicotineLabel}
+          ref={components.nicotine.ref}
+          onMouseEnter={() => setTooltipVisibility('nicotine', true)}
+          onMouseLeave={() => setTooltipVisibility('nicotine', false)}
+        />
+        <ProgressBar
+          now={flavor}
+          label={flavorLabel}
+          ref={components.flavor.ref}
+          onMouseEnter={() => setTooltipVisibility('flavor', true)}
+          onMouseLeave={() => setTooltipVisibility('flavor', false)}
+        />
+        <ProgressBar
+          now={vg}
+          variant="success"
+          label={vgLabel}
+          ref={components.vg.ref}
+          onMouseEnter={() => setTooltipVisibility('vg', true)}
+          onMouseLeave={() => setTooltipVisibility('vg', false)}
+        />
+        <ProgressBar
+          now={pg}
+          variant="warning"
+          label={pgLabel}
+          ref={components.pg.ref}
+          onMouseEnter={() => setTooltipVisibility('pg', true)}
+          onMouseLeave={() => setTooltipVisibility('pg', false)}
+        />
+      </ProgressBar>
+      {getTooltip('nicotine')}
+      {getTooltip('flavor')}
+      {getTooltip('vg')}
+      {getTooltip('pg')}
+    </Fragment>
+  );
 }
+
+IngredientBar.propTypes = {
+  nicotine: PropTypes.number.isRequired,
+  flavor: PropTypes.number.isRequired,
+  vg: PropTypes.number.isRequired,
+  pg: PropTypes.number.isRequired
+};
+
+IngredientBar.defaultProps = {
+  nicotine: 0,
+  flavor: 0,
+  vg: 0,
+  pg: 0
+};
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,6 @@
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useCallback } from 'react';
 import { Form as FinalForm, Field } from 'react-final-form';
 import { Button, Container, Form, InputGroup, Col } from 'react-bootstrap';
 
@@ -16,226 +14,200 @@ import {
   length
 } from 'utils/validation';
 
-export class Register extends Component {
-  static propTypes = {
-    registering: PropTypes.bool.isRequired,
-    actions: PropTypes.shape({
-      registerUser: PropTypes.func.isRequired
-    })
-  };
+export default function Register() {
+  const dispatch = useDispatch();
+  const registering = useSelector(isRegistering);
+  const handleSubmit = useCallback(
+    (values) => dispatch(appActions.registerUser(values)),
+    [dispatch]
+  );
 
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(values) {
-    const { actions } = this.props;
-
-    actions.registerUser(values);
-  }
-
-  render() {
-    const { registering } = this.props;
-
-    return (
-      <Container>
-        <Helmet title="Register" />
-        <h1>User Registration</h1>
-        <FinalForm
-          onSubmit={this.handleSubmit}
-          render={({ handleSubmit, submitting }) => (
-            <Form noValidate onSubmit={handleSubmit}>
-              <Form.Row>
-                <Field
-                  name="username"
-                  validate={composeValidators(required, length(3, 50))}
-                >
-                  {({ input, meta }) => (
-                    <Form.Group as={Col} md="4">
-                      <Form.Label>Username</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text id="inputGroupPrepend">
-                            @
-                          </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          {...input}
-                          type="text"
-                          placeholder="Username"
-                          aria-describedby="inputGroupPrepend"
-                          isInvalid={meta.error && meta.touched}
-                        />
-                        {meta.touched && meta.error && (
-                          <Form.Control.Feedback type="invalid">
-                            {meta.error === 'required'
-                              ? 'Please enter a username'
-                              : 'Username must be between 3 and 50 characters'}
-                          </Form.Control.Feedback>
-                        )}
-                      </InputGroup>
-                    </Form.Group>
-                  )}
-                </Field>
-              </Form.Row>
-              <Form.Row>
-                <Field
-                  name="emailAddress"
-                  validate={composeValidators(required, email)}
-                >
-                  {({ input, meta }) => (
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Email Address</Form.Label>
+  return (
+    <Container>
+      <Helmet title="Register" />
+      <h1>User Registration</h1>
+      <FinalForm
+        onSubmit={handleSubmit}
+        render={({ handleSubmit: submit, submitting }) => (
+          <Form noValidate onSubmit={submit}>
+            <Form.Row>
+              <Field
+                name="username"
+                validate={composeValidators(required, length(3, 50))}
+              >
+                {({ input, meta }) => (
+                  <Form.Group as={Col} md="4">
+                    <Form.Label>Username</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          @
+                        </InputGroup.Text>
+                      </InputGroup.Prepend>
                       <Form.Control
                         {...input}
-                        type="email"
-                        placeholder="Enter email"
+                        type="text"
+                        placeholder="Username"
+                        aria-describedby="inputGroupPrepend"
                         isInvalid={meta.error && meta.touched}
                       />
                       {meta.touched && meta.error && (
                         <Form.Control.Feedback type="invalid">
                           {meta.error === 'required'
-                            ? 'Please enter your email address'
-                            : 'Please enter a valid email address'}
+                            ? 'Please enter a username'
+                            : 'Username must be between 3 and 50 characters'}
                         </Form.Control.Feedback>
                       )}
-                      <Form.Text className="text-muted">
-                        We&apos;ll never share your email with anyone else.
-                      </Form.Text>
-                    </Form.Group>
-                  )}
-                </Field>
-                <Field
-                  name="emailAddressConfirm"
-                  validate={composeValidators(
-                    required,
-                    email,
-                    matches('emailAddress')
-                  )}
+                    </InputGroup>
+                  </Form.Group>
+                )}
+              </Field>
+            </Form.Row>
+            <Form.Row>
+              <Field
+                name="emailAddress"
+                validate={composeValidators(required, email)}
+              >
+                {({ input, meta }) => (
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                      {...input}
+                      type="email"
+                      placeholder="Enter email"
+                      isInvalid={meta.error && meta.touched}
+                    />
+                    {meta.touched && meta.error && (
+                      <Form.Control.Feedback type="invalid">
+                        {meta.error === 'required'
+                          ? 'Please enter your email address'
+                          : 'Please enter a valid email address'}
+                      </Form.Control.Feedback>
+                    )}
+                    <Form.Text className="text-muted">
+                      We&apos;ll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+                )}
+              </Field>
+              <Field
+                name="emailAddressConfirm"
+                validate={composeValidators(
+                  required,
+                  email,
+                  matches('emailAddress')
+                )}
+              >
+                {({ input, meta }) => (
+                  <Form.Group as={Col} md="6" controlId="emailConf">
+                    <Form.Label>Confirm Email Address</Form.Label>
+                    <Form.Control
+                      {...input}
+                      type="email"
+                      placeholder="Confirm Enter email"
+                      isInvalid={meta.error && meta.touched}
+                    />
+                    {meta.touched && meta.error === 'required' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please re-enter your email address
+                      </Form.Control.Feedback>
+                    )}
+                    {meta.touched && meta.error === 'matches' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please ensure both email addresses match
+                      </Form.Control.Feedback>
+                    )}
+                    {meta.touched && meta.error === 'email' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please enter a valid email address
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                )}
+              </Field>
+            </Form.Row>
+            <Form.Row>
+              <Field
+                name="password"
+                validate={composeValidators(required, length(8))}
+              >
+                {({ input, meta }) => (
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      {...input}
+                      type="password"
+                      placeholder="Enter Password"
+                      isInvalid={meta.error && meta.touched}
+                    />
+                    {meta.touched && meta.error === 'required' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please enter a password
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                )}
+              </Field>
+              <Field
+                name="passwordConfirm"
+                validate={composeValidators(required, matches('password'))}
+              >
+                {({ input, meta }) => (
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                      {...input}
+                      type="password"
+                      placeholder="Confirm Password"
+                      isInvalid={meta.error && meta.touched}
+                    />
+                    {meta.touched && meta.error === 'required' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please re-enter your password
+                      </Form.Control.Feedback>
+                    )}
+                    {meta.touched && meta.error === 'matches' && (
+                      <Form.Control.Feedback type="invalid">
+                        Please ensure both passwords match
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                )}
+              </Field>
+            </Form.Row>
+            <Form.Row>
+              <Field type="checkbox" name="termsAgreed" validate={required}>
+                {({ input, meta }) => (
+                  <Form.Group>
+                    <Form.Check
+                      {...input}
+                      label="I agree to comply with the Terms of Service"
+                      isInvalid={meta.error && meta.touched}
+                    />
+                    {meta.touched && meta.error && (
+                      <Form.Control.Feedback type="invalid">
+                        Please accept the Terms of Service
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                )}
+              </Field>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} md="2">
+                <Button
+                  className="button-animation"
+                  variant="primary"
+                  type="submit"
+                  disabled={submitting && !registering}
                 >
-                  {({ input, meta }) => (
-                    <Form.Group as={Col} md="6" controlId="emailConf">
-                      <Form.Label>Confirm Email Address</Form.Label>
-                      <Form.Control
-                        {...input}
-                        type="email"
-                        placeholder="Confirm Enter email"
-                        isInvalid={meta.error && meta.touched}
-                      />
-                      {meta.touched && meta.error === 'required' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please re-enter your email address
-                        </Form.Control.Feedback>
-                      )}
-                      {meta.touched && meta.error === 'matches' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please ensure both email addresses match
-                        </Form.Control.Feedback>
-                      )}
-                      {meta.touched && meta.error === 'email' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please enter a valid email address
-                        </Form.Control.Feedback>
-                      )}
-                    </Form.Group>
-                  )}
-                </Field>
-              </Form.Row>
-              <Form.Row>
-                <Field
-                  name="password"
-                  validate={composeValidators(required, length(8))}
-                >
-                  {({ input, meta }) => (
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        {...input}
-                        type="password"
-                        placeholder="Enter Password"
-                        isInvalid={meta.error && meta.touched}
-                      />
-                      {meta.touched && meta.error === 'required' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please enter a password
-                        </Form.Control.Feedback>
-                      )}
-                    </Form.Group>
-                  )}
-                </Field>
-                <Field
-                  name="passwordConfirm"
-                  validate={composeValidators(required, matches('password'))}
-                >
-                  {({ input, meta }) => (
-                    <Form.Group as={Col} md="6">
-                      <Form.Label>Confirm Password</Form.Label>
-                      <Form.Control
-                        {...input}
-                        type="password"
-                        placeholder="Confirm Password"
-                        isInvalid={meta.error && meta.touched}
-                      />
-                      {meta.touched && meta.error === 'required' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please re-enter your password
-                        </Form.Control.Feedback>
-                      )}
-                      {meta.touched && meta.error === 'matches' && (
-                        <Form.Control.Feedback type="invalid">
-                          Please ensure both passwords match
-                        </Form.Control.Feedback>
-                      )}
-                    </Form.Group>
-                  )}
-                </Field>
-              </Form.Row>
-              <Form.Row>
-                <Field type="checkbox" name="termsAgreed" validate={required}>
-                  {({ input, meta }) => (
-                    <Form.Group>
-                      <Form.Check
-                        {...input}
-                        label="I agree to comply with the Terms of Service"
-                        isInvalid={meta.error && meta.touched}
-                      />
-                      {meta.touched && meta.error && (
-                        <Form.Control.Feedback type="invalid">
-                          Please accept the Terms of Service
-                        </Form.Control.Feedback>
-                      )}
-                    </Form.Group>
-                  )}
-                </Field>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md="2">
-                  <Button
-                    className="button-animation"
-                    variant="primary"
-                    type="submit"
-                    disabled={submitting && !registering}
-                  >
-                    <span>Register</span>
-                  </Button>
-                </Form.Group>
-              </Form.Row>
-            </Form>
-          )}
-        />
-      </Container>
-    );
-  }
+                  <span>Register</span>
+                </Button>
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        )}
+      />
+    </Container>
+  );
 }
-
-const mapStateToProps = (state) => ({
-  registering: isRegistering(state)
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(appActions, dispatch)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Register);
