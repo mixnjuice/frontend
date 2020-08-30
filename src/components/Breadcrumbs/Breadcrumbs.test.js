11c899b643eb0a0f8b89f6e4fe9a9e2dc42cfa6c
daviddyess <david.dyess@gmail.com>
2020-08-30 00:32:59 -0600

    Added Breadcrumbs component

---
+++ src/components/Breadcrumbs/Breadcrumbs.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Breadcrumbs from './Breadcrumbs';
+import { withMemoryRouter } from 'utils/testing';
+// Prevent findDOMNode error in test from Dropdown component in react-bootstrap
+jest.mock('react-dom', () => ({
+  findDOMNode: () => ({})
+}));
+
+describe('<Breadcrumbs />', () => {
+  const RoutedBreadcrumbs = withMemoryRouter(Breadcrumbs);
+
+  it('renders home correctly', () => {
+    const props = {
+      base: 'home',
+      active: 'User Stuff'
+    };
+
+    expect(
+      renderer.create(<RoutedBreadcrumbs {...props} />).toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('renders admin in correctly', () => {
+    const props = {
+      base: 'admin',
+      links: [{ name: 'Admin Page', url: '/admin/stuff' }],
+      active: 'Admin Stuff'
+    };
+
+    expect(
+      renderer.create(<RoutedBreadcrumbs {...props} />).toJSON()
+    ).toMatchSnapshot();
+  });
+});
