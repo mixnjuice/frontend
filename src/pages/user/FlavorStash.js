24fb468ec664592d9627d456d59de4358a23b563
ayan4m1 <andrew@bulletlogic.com>
2020-10-09 17:41:18 -0400

    continue porting components to functional style

--- src/pages/user/FlavorStash.js
+++ src/pages/user/FlavorStash.js
@@ -1,75 +1,52 @@
 import dayjs from 'dayjs';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Container, Table } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { getStash, isLoaded } from 'selectors/flavor';
 import { actions as flavorActions } from 'reducers/flavor';
 
-export class FlavorStash extends Component {
-  static propTypes = {
-    actions: PropTypes.shape({
-      requestStash: PropTypes.func.isRequired
-    }).isRequired,
-    stash: PropTypes.arrayOf(PropTypes.object),
-    stashLoaded: PropTypes.bool.isRequired
-  };
+export default function FlavorStash() {
+  const dispatch = useDispatch();
+  const stash = useSelector(getStash);
+  const stashLoaded = useSelector(isLoaded);
 
-  componentDidMount() {
-    const { actions, stashLoaded } = this.props;
-
+  useEffect(() => {
     if (!stashLoaded) {
-      actions.requestStash();
+      dispatch(flavorActions.requestStash());
     }
-  }
+  }, [stashLoaded, dispatch]);
 
-  render() {
-    const { stash } = this.props;
+  const hasStash = Array.isArray(stash) && stash.length;
 
-    const hasStash = Array.isArray(stash) && stash.length;
-
-    return (
-      <Container>
-        <Helmet title="User Flavor Stash" />
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>Vendor</th>
-              <th>Flavor</th>
-              <th>Added</th>
-            </tr>
-          </thead>
-          <tbody>
-            {hasStash ? (
-              stash.map((flavor, index) => (
-                <tr key={index}>
-                  <td>{flavor?.Flavor?.Vendor?.code}</td>
-                  <td>{flavor?.Flavor.name}</td>
-                  <td>{dayjs(flavor?.created).format('YYYY-MM-DD')}</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={4}>No flavors in stash.</td>
+  return (
+    <Container>
+      <Helmet title="User Flavor Stash" />
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>Vendor</th>
+            <th>Flavor</th>
+            <th>Added</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hasStash ? (
+            stash.map((flavor, index) => (
+              <tr key={index}>
+                <td>{flavor?.Flavor?.Vendor?.code}</td>
+                <td>{flavor?.Flavor.name}</td>
+                <td>{dayjs(flavor?.created).format('YYYY-MM-DD')}</td>
               </tr>
-            )}
-          </tbody>
-        </Table>
-      </Container>
-    );
-  }
+            ))
+          ) : (
+            <tr>
+              <td colSpan={4}>No flavors in stash.</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
-
-export const mapStateToProps = (state) => ({
-  stash: getStash(state),
-  stashLoaded: isLoaded(state)
-});
-
-export const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(flavorActions, dispatch)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(FlavorStash);
